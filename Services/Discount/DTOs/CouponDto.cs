3cfa0b74d2dd5a3a9fa1649f0b828713d9540e11
edobasky <edore7@gmail.com>
2026-04-05 12:03:44 +0100

    Added  Discount GRPC Service

--- Services/Discount/DTOs/CouponDto.cs
+++ Services/Discount/DTOs/CouponDto.cs
@@ -1,5 +1,5 @@
 ﻿namespace Discount.DTOs
 {
-    public record CouponDto(int id, string ProductName, string Description, int Amount);
+    public record CouponDto(int Id, string ProductName, string Description, int Amount);
   
 }
